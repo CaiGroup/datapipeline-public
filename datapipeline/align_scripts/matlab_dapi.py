509da05ebc314fa53d63e1a02b7048643fc4f9bc
Lincoln Ombelets <lombelets@caltech.edu>
2021-08-24 13:48:08 -0700

    fix directory nonsense in old matlab_dapi.py

--- datapipeline/align_scripts/matlab_dapi.py
+++ datapipeline/align_scripts/matlab_dapi.py
@@ -20,12 +20,8 @@ def matlab_dapi(fixed_image_src, moving_image_src, num_wav, rand_dir):
     
     #Create Paths to add
     #-------------------------------------------------------------------
-    cwd = os.getcwd()
-    
-    cwd = cwd[cwd.find('/home'):]
-
-    matlab_dapi_dir = os.path.join(cwd, 'align_scripts','matlab_dapi')
-    
+    folder = os.path.dirname(__file__)
+    matlab_dapi_dir = os.path.join(folder, 'matlab_dapi')
     bfmatlab_dir = os.path.join(matlab_dapi_dir, 'bfmatlab')
     #-------------------------------------------------------------------
     
@@ -89,53 +85,53 @@ def matlab_dapi(fixed_image_src, moving_image_src, num_wav, rand_dir):
     #------------------------------------------------------------------
     
 
+if __name__ == '__main__':
+    if 'debug' not in sys.argv[1]:
+        import argparse
 
-if 'debug' not in sys.argv[1]:
-    import argparse
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--fixed_src")
-    parser.add_argument("--moving_src")
-    parser.add_argument("--rand_dir")
-    parser.add_argument("--num_wav")
-    
-    args, unknown = parser.parse_known_args()
-    
-    matlab_dapi(args.fixed_src, args.moving_src, args.num_wav, args.rand_dir)
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--fixed_src")
+        parser.add_argument("--moving_src")
+        parser.add_argument("--rand_dir")
+        parser.add_argument("--num_wav")
 
-elif sys.argv[1] == 'debug_matlab_dapi':
-    
-    fixed_src = '/groups/CaiLab/personal/alinares/raw/2021_0512_mouse_hydrogel/HybCycle_2/MMStack_Pos0.ome.tif'
-    moving_src = '/groups/CaiLab/personal/alinares/raw/2021_0512_mouse_hydrogel/HybCycle_37/MMStack_Pos0.ome.tif'
-    
-    rand_dir = 'foo/matlab_dapi'
-    os.makedirs(rand_dir, exist_ok=True)
-    num_wav = 4
-    start_time = None
-    
-    matlab_dapi(fixed_src, moving_src, num_wav, rand_dir)
-    
-elif sys.argv[1] == 'debug_matlab_dapi_jina':
-    
-    fixed_src = '/groups/CaiLab/personal/nrezaee/raw/jina_1_pseudos_4_corrected/HybCycle_4/MMStack_Pos6.ome.tif'
-    moving_src = '/groups/CaiLab/personal/nrezaee/raw/jina_1_pseudos_4_corrected/HybCycle_5/MMStack_Pos6.ome.tif'
-    
-    rand_dir = 'foo/matlab_dapi'
-    os.makedirs(rand_dir, exist_ok=True)
-    num_wav = 4
-    start_time = None
-    
-    matlab_dapi(fixed_src, moving_src, num_wav, rand_dir)
+        args, unknown = parser.parse_known_args()
 
-elif sys.argv[1] == 'debug_matlab_dapi_1z':
-    
-    fixed_src = '/groups/CaiLab/personal/Michal/raw/2021-06-21_Neuro4181_5_noGel_pool1/HybCycle_9/MMStack_Pos33.ome.tif'
-    moving_src = '/groups/CaiLab/personal/Michal/raw/2021-06-21_Neuro4181_5_noGel_pool1/final_background/MMStack_Pos33.ome.tif'
-    
-    rand_dir = 'foo/matlab_dapi'
-    os.makedirs(rand_dir, exist_ok=True)
-    num_wav = 4
-    start_time = None
-    
-    matlab_dapi(fixed_src, moving_src, num_wav, rand_dir)
+        matlab_dapi(args.fixed_src, args.moving_src, args.num_wav, args.rand_dir)
+
+    elif sys.argv[1] == 'debug_matlab_dapi':
+
+        fixed_src = '/groups/CaiLab/personal/alinares/raw/2021_0512_mouse_hydrogel/HybCycle_2/MMStack_Pos0.ome.tif'
+        moving_src = '/groups/CaiLab/personal/alinares/raw/2021_0512_mouse_hydrogel/HybCycle_37/MMStack_Pos0.ome.tif'
+
+        rand_dir = 'foo/matlab_dapi'
+        os.makedirs(rand_dir, exist_ok=True)
+        num_wav = 4
+        start_time = None
+
+        matlab_dapi(fixed_src, moving_src, num_wav, rand_dir)
+
+    elif sys.argv[1] == 'debug_matlab_dapi_jina':
+
+        fixed_src = '/groups/CaiLab/personal/nrezaee/raw/jina_1_pseudos_4_corrected/HybCycle_4/MMStack_Pos6.ome.tif'
+        moving_src = '/groups/CaiLab/personal/nrezaee/raw/jina_1_pseudos_4_corrected/HybCycle_5/MMStack_Pos6.ome.tif'
+
+        rand_dir = 'foo/matlab_dapi'
+        os.makedirs(rand_dir, exist_ok=True)
+        num_wav = 4
+        start_time = None
+
+        matlab_dapi(fixed_src, moving_src, num_wav, rand_dir)
+
+    elif sys.argv[1] == 'debug_matlab_dapi_1z':
+
+        fixed_src = '/groups/CaiLab/personal/Michal/raw/2021-06-21_Neuro4181_5_noGel_pool1/HybCycle_9/MMStack_Pos33.ome.tif'
+        moving_src = '/groups/CaiLab/personal/Michal/raw/2021-06-21_Neuro4181_5_noGel_pool1/final_background/MMStack_Pos33.ome.tif'
+
+        rand_dir = 'foo/matlab_dapi'
+        os.makedirs(rand_dir, exist_ok=True)
+        num_wav = 4
+        start_time = None
+
+        matlab_dapi(fixed_src, moving_src, num_wav, rand_dir)
     
